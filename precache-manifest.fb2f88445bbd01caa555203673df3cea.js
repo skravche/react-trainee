556dbd7f56216baf4c62e56f6ffe829018f2116a
skravche <sktechno92@gmail.com>
2019-06-29 16:35:13 +0300

    Updates

---
+++ precache-manifest.fb2f88445bbd01caa555203673df3cea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6858bbaa8702ab06df327bb68bbc3c5e",
+    "url": "/react-trainee/index.html"
+  },
+  {
+    "revision": "88c5eb4e663595e9cf9e",
+    "url": "/react-trainee/static/css/main.b72ec3be.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/react-trainee/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "88c5eb4e663595e9cf9e",
+    "url": "/react-trainee/static/js/main.02855ac4.chunk.js"
+  },
+  {
+    "revision": "cc21b92b3a4e91c9c2dc",
+    "url": "/react-trainee/static/js/runtime~main.d4b69caa.js"
+  }
+]);
